e1eddffe65a092cadf9c9b86e06b4f9ce8ea64d6
Elias W. BA <eliaswalyba@gmail.com>
2025-10-21 01:12:06 +0000

    Clean up

--- platform/src/server.ts
+++ platform/src/server.ts
@@ -8,7 +8,7 @@ import { html } from "@elysiajs/html";
 export default async (port: number | string = 3000) => {
   const app = new Elysia({
     serve: {
-      idleTimeout: 255, // Max timeout (255 seconds = 4.25 minutes) for streaming responses
+      idleTimeout: 255,
     },
   });
 
