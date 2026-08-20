6a285a0e6c8fe46a929e7afac768dfe861adbe76
Joe Clark <jclark@openfn.org>
2026-08-20 13:50:46 +0100

    make output path more robust

--- services/entry.py
+++ services/entry.py
@@ -3,6 +3,7 @@ import os
 import json
 import uuid
 import argparse
+from pathlib import Path
 from dotenv import load_dotenv
 import sentry_sdk
 from util import set_apollo_port, ApolloError
@@ -96,6 +97,7 @@ def call(
     langfuse.flush()
 
     if output_path:
+        Path(os.path.dirname(output_path)).mkdir(parents=True, exist_ok=True)
         with open(output_path, "w") as f:
             json.dump(result, f)
 
